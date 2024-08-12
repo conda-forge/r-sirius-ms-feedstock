1cdc635a35b191b11eb47a640e8b9d23b4b6eb73
joXemMx <jonas.em@t-online.de>
2024-08-12 14:50:17 +0200

    fix bugs in test script

--- recipe/test_script.R
+++ recipe/test_script.R
@@ -18,7 +18,7 @@ sirius_api$projects_api$CreateProjectSpace(project_id, project_dir)
 data <- file.path(Sys.getenv('SRC_DIR'),"Kaempferol.ms")
 sirius_api$projects_api$ImportPreprocessedData(project_id, input_files=data)
 
-job_submission <- sirius_api$GetDefaultJobConfig()
+job_submission <- sirius_api$jobs_api$GetDefaultJobConfig()
 job_submission$spectraSearchParams$enabled <- FALSE
 job_submission$formulaIdParams$enabled <- TRUE
 job_submission$fingerprintPredictionParams$enabled <- FALSE
@@ -31,8 +31,8 @@ wait_for_job(project_id, job)
 aligned_feature_id <- sirius_api$features_api$GetAlignedFeatures(project_id)[[1]]$alignedFeatureId
 formula_id <- sirius_api$features_api$GetFormulaCandidates(project_id, aligned_feature_id)[[1]]$formulaId
 tree <- sirius_api$features_api$GetFragTree(project_id, aligned_feature_id, formula_id)
-write(tree.toJSON(), "test_fragtree.json")
+write(tree$toJSONString(), "test_fragtree.json")
 
 sirius_api$projects_api$CloseProjectSpace(project_id)
 unlink(project_dir, recursive=TRUE)
-sdk$shutdown()
+sdk$shutdown_sirius()
