048c3ccfa3eb618a596f70f7cd812870e49ec0a6
Jonas Emmert <63006948+joXemMx@users.noreply.github.com>
2024-08-12 16:01:15 +0200

    adjust for sirius6 (#11)
    * adjust for sirius6
    
    * MNT: Re-rendered with conda-build 24.7.1, conda-smithy 3.37.2, and conda-forge-pinning 2024.08.09.16.11.22
    
    * correct sha256
    
    * update sirius-ms to 6.0.4
    
    * update hash, use sirius-ms 6.0.4
    
    * fix bugs in test script
    
    * update hash + url
    
    * update hash
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>
    Co-authored-by: Markus Fleischauer <markus.fleischauer@uni-jena.de>

--- recipe/test_script.R
+++ recipe/test_script.R
@@ -3,21 +3,36 @@ options(error = traceback)
 library('Rsirius')
 
 sdk <- SiriusSDK$new()
-wrapper <- sdk$start()
+sirius_api <- sdk$attach_or_start_sirius()
 
-wait_for_job <- function(pid, job) {
-  while (!(wrapper$computations_api$GetJob(pid, job$id)$progress$state == "DONE")) {
+wait_for_job <- function(project_id, job) {
+  while (sirius_api$jobs_api$GetJob(project_id, job$id)$progress$state != "DONE") {
     Sys.sleep(1)
   }
 }
 
-pspace <- wrapper$project_spaces_api$GetProjectSpaces()[[1]]$name
+project_id <- "test_project"
+project_dir <- paste(Sys.getenv('SRC_DIR'), project_id, sep="/")
+sirius_api$projects_api$CreateProjectSpace(project_id, project_dir)
+
 data <- file.path(Sys.getenv('SRC_DIR'),"Kaempferol.ms")
-wrapper$compounds_api$ImportCompounds(pspace, c(data))
-config <- wrapper$computations_api$GetDefaultJobConfig()
-compoundId <- "1_Kaempferol_Kaempferol"
-formulaId <- "C15H10O6_[M+H]+"
-compute_job <- wrapper$computations_api$StartJob(pspace, config)
-wait_for_job(pspace, compute_job)
-wrapper$formula_results_api$GetFragTree(pspace, compoundId, formulaId, data_file=file.path(".","test_fragtree.txt"))
-sdk$shutdown()
+sirius_api$projects_api$ImportPreprocessedData(project_id, input_files=data)
+
+job_submission <- sirius_api$jobs_api$GetDefaultJobConfig()
+job_submission$spectraSearchParams$enabled <- FALSE
+job_submission$formulaIdParams$enabled <- TRUE
+job_submission$fingerprintPredictionParams$enabled <- FALSE
+job_submission$structureDbSearchParams$enabled <- FALSE
+job_submission$canopusParams$enabled <- FALSE
+job_submission$msNovelistParams$enabled <- FALSE
+job <- sirius_api$jobs_api$StartJob(project_id, job_submission)
+wait_for_job(project_id, job)
+
+aligned_feature_id <- sirius_api$features_api$GetAlignedFeatures(project_id)[[1]]$alignedFeatureId
+formula_id <- sirius_api$features_api$GetFormulaCandidates(project_id, aligned_feature_id)[[1]]$formulaId
+tree <- sirius_api$features_api$GetFragTree(project_id, aligned_feature_id, formula_id)
+write(tree$toJSONString(), "test_fragtree.json")
+
+sirius_api$projects_api$CloseProjectSpace(project_id)
+unlink(project_dir, recursive=TRUE)
+sdk$shutdown_sirius()
